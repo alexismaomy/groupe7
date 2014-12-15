2f08d7c3427abcd2629c063721b77074cbf70c51
diasse85 <alexis.maomy@yahoo.fr>
2014-12-15 22:55:34 +0100

    Amélioration du code de l'activité CreerActivity

--- src/be/condorcet/geolocalisation/CreerActivity.java
+++ src/be/condorcet/geolocalisation/CreerActivity.java
@@ -5,7 +5,11 @@ import java.util.ArrayList;
 
 import myconnections.DBConnection;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -18,17 +22,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+import entities.CategorieDB;
 import entities.PointInteretDB;
 
 public class CreerActivity extends ActionBarActivity {
 	private Connection con = null;
 	private String msgcreation, patientez, echec, champvide, ptexistedeja,
-			creerpt, pascreer;
+			creerpt, pascreer, categorie, ptcree;
 	private EditText ednom, eddescription, edlongitude, edcategorie,
 			edlatitude;
-	private boolean unique;
+	private boolean unique = false;
+	public static final String IDUSER = "userid";
+	private int iduser;
 	public static final String CAT = "cat";
-	ArrayList<String> c;
+	private Button btcreerpt;
+	private ArrayList<String> c;
+	private double longitude, latitude;
+
+	private LocationManager locationManager;
+
+	private EditText edDegLat, edMinLat, edSecLat, edDegLong, edMinLong,
+			edSecLong, edDist;
+
+	private Point ancienPt = new Point(0, 0), pt = new Point(0, 0);
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,25 +53,125 @@ public class CreerActivity extends ActionBarActivity {
 
 		ednom = (EditText) findViewById(R.id.ednom);
 		eddescription = (EditText) findViewById(R.id.eddescription);
-		edlongitude = (EditText) findViewById(R.id.edlongitude);
-		edlatitude = (EditText) findViewById(R.id.edlatitude);
+
+		edDegLat = (EditText) findViewById(R.id.degLat);
+		edDegLat.setEnabled(false);
+		edMinLat = (EditText) findViewById(R.id.minLat);
+		edMinLat.setEnabled(false);
+		edSecLat = (EditText) findViewById(R.id.secLat);
+		edSecLat.setEnabled(false);
+		edDegLong = (EditText) findViewById(R.id.degLong);
+		edDegLong.setEnabled(false);
+		edMinLong = (EditText) findViewById(R.id.minLong);
+		edMinLong.setEnabled(false);
+		edSecLong = (EditText) findViewById(R.id.secLong);
+		edSecLong.setEnabled(false);
+
 		edcategorie = (EditText) findViewById(R.id.edcategorie);
 		champvide = getResources().getString(R.string.champvide);
 		pascreer = getResources().getString(R.string.pascreer);
+		ptcree = getResources().getString(R.string.ptcree);
 		ptexistedeja = getResources().getString(R.string.ptexistedeja);
 		creerpt = getResources().getString(R.string.creerpt);
 		echec = getResources().getString(R.string.echec);
+		msgcreation = getResources().getString(R.string.msgcreation);
 		patientez = getResources().getString(R.string.patientez);
+
+		btcreerpt = (Button) findViewById(R.id.btcreerpoint);
+		btcreerpt.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// edmsg.setText(authentifier);
+				MyAccesDBPointInteret adb = new MyAccesDBPointInteret(
+						CreerActivity.this);
+				adb.execute();
+				ancienPt = pt;
+			}
+		});
+
+		// ArrayList sauvegardes = (ArrayList)getLastNonConfigurationInstance();
+		ArrayList sauvegardes = (ArrayList) getLastCustomNonConfigurationInstance();
+		if (sauvegardes != null) {
+			ancienPt = (Point) sauvegardes.get(0);
+			pt = (Point) sauvegardes.get(1);
+
+		}
+
+		// récupération des services de localisation disponibles (GPS,signal
+		// GSM,...)
+		ArrayList<LocationProvider> providers = new ArrayList<LocationProvider>();
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		ArrayList<String> names = (ArrayList<String>) locationManager
+				.getProviders(true);
+		for (String name : names)
+			providers.add(locationManager.getProvider(name));
+
+		// locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER,
+		// 1000, 0,new LocationListener()
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				1000, 0, new LocationListener() {
+
+					@Override
+					public void onLocationChanged(
+							android.location.Location location) {
+						// TODO Auto-generated method stub
+						if (location != null) {
+
+							longitude = location.getLongitude();
+							latitude = location.getLatitude();
+							pt = new Point(latitude, longitude);
+
+							edDegLat.setText("" + pt.deglat);
+							edMinLat.setText("" + pt.minlat);
+							edSecLat.setText("" + pt.seclat + "-"
+									+ pt.centiseclat);
+
+							edDegLong.setText("" + pt.deglong);
+							edMinLong.setText("" + pt.minlong);
+							edSecLong.setText("" + pt.seclong + "-"
+									+ pt.centiseclong);
+							// long dist = pt.dist(ancienPt);
+							// edDist.setText("" + dist);
+
+						}
+
+					}
+
+					@Override
+					public void onProviderDisabled(String provider) {
+						// TODO Auto-generated method stub
+
+						// Pass views as parameters? DIscuss
+						// DeviceName.setText(String.valueOf(Latitude)
+						// +" "+String.valueOf(Longitude));
+
+					}
+
+					@Override
+					public void onProviderEnabled(String provider) {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public void onStatusChanged(String provider, int status,
+							Bundle extras) {
+						// TODO Auto-generated method stub
+
+					}
+
+				});
 
 		Intent i = getIntent();
 		c = i.getStringArrayListExtra(MenuActivity.CAT);
-
+		iduser = i.getIntExtra(MenuActivity.IDUSER, 0);
 		final Spinner list = (Spinner) findViewById(R.id.spin);
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_spinner_item);
+				android.R.layout.simple_spinner_item, c);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		list.setAdapter(adapter);
 		final Button active = (Button) findViewById(R.id.activation);
+
 		active.setOnClickListener(new Button.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -80,6 +196,67 @@ public class CreerActivity extends ActionBarActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.creer, menu);
 		return true;
+	}
+
+	public Object onRetainCustomNonConfigurationInstance() {
+		ArrayList sauvegardes = new ArrayList();
+		sauvegardes.add(ancienPt);
+		sauvegardes.add(pt);
+		return sauvegardes;
+	}
+
+	class Point {
+		int deglat, minlat, seclat, centiseclat;
+		int deglong, minlong, seclong, centiseclong;
+		double latitude, longitude;
+
+		public Point(double latitude, double longitude) {
+			this.latitude = latitude;
+			this.longitude = longitude;
+
+			double valeur = latitude;
+			int valConv = (int) (valeur * 3600);
+			centiseclat = (int) ((valeur * 3600 - valConv) * 100);
+			deglat = valConv / 3600;
+			valConv = valConv % 3600;
+			minlat = valConv / 60;
+			seclat = valConv % 60;
+
+			valeur = longitude;
+			valConv = (int) (valeur * 3600);
+			centiseclong = (int) ((valeur * 3600 - valConv) * 100);
+			deglong = valConv / 3600;
+			valConv = valConv % 3600;
+			minlong = valConv / 60;
+			seclong = valConv % 60;
+
+		}
+
+		public Point(int deglat, int minlat, int seclat, int centiseclat,
+				int deglong, int minlong, int seclong, int centiseclong) {
+			super();
+			this.deglat = deglat;
+			this.minlat = minlat;
+			this.seclat = seclat;
+			this.centiseclat = centiseclat;
+			this.deglong = deglong;
+			this.minlong = minlong;
+			this.seclong = seclong;
+			this.centiseclong = centiseclong;
+			latitude = (deglat * 3600 + minlat * 60 + seclat + centiseclat / 100.0) / 3600;
+			longitude = (deglong * 3600 + minlong * 60 + seclong + centiseclong / 100.0) / 3600;
+
+		}
+
+		public long dist(Point pt) {// formules approximatives
+			double dlat = Math.abs(latitude - pt.latitude) / 90 * 10000000;
+			double latInvRadians = Math.abs(90 - latitude) / 180 * Math.PI;
+			double circLat = 10000000 * Math.sin(latInvRadians);
+			double dlong = Math.abs(longitude - pt.longitude) / 90 * circLat;
+			double distance = Math.sqrt(dlat * dlat + dlong * dlong);
+			return (long) distance;
+		}
+
 	}
 
 	@Override
@@ -110,7 +287,8 @@ public class CreerActivity extends ActionBarActivity {
 		private String resultat;
 		private ProgressDialog pgd = null;
 		private String nom, description;
-		private double longitude, latitude;
+		// private double longitude, latitude;
+		private boolean flag = false;
 
 		public MyAccesDBPointInteret(CreerActivity pActivity) {
 			link(pActivity);
@@ -139,17 +317,31 @@ public class CreerActivity extends ActionBarActivity {
 					return false;
 				}
 				PointInteretDB.setConnection(con);
+				CategorieDB.setConnection(con);
 			}
 			// int id=Integer.parseInt(ed.getText().toString());
 			nom = ednom.getText().toString();
 			description = eddescription.getText().toString();
-			longitude = Double.parseDouble(edlongitude.getText().toString());
-			latitude = Double.parseDouble(edlatitude.getText().toString());
+			categorie = edcategorie.getText().toString();
+			// longitude = Double.parseDouble(edlongitude.getText().toString());
+			// latitude = Double.parseDouble(edlatitude.getText().toString());
 			try {
-				PointInteretDB p = new PointInteretDB(nom, description,
+				PointInteretDB p = new PointInteretDB(iduser, nom, description,
 						longitude, latitude);
 				p.create();
-				p.read();
+				// p.read();
+				int cpt = 0;
+				do {
+					if (c.get(cpt).equalsIgnoreCase(categorie)) {
+						flag = true;
+					}
+					cpt++;
+				} while (flag == false && cpt < c.size());
+				if (!flag) {
+					CategorieDB cat = new CategorieDB(categorie);
+					cat.create();
+					c = CategorieDB.liste();
+				}
 				unique = true;
 				// resultat = u.toString();
 			} catch (Exception e) {
@@ -169,8 +361,13 @@ public class CreerActivity extends ActionBarActivity {
 				return;
 			}
 			if (nom.trim().equals("") || description.trim().equals("")
-					|| edlongitude.getText().toString().trim().equals("")
-					|| edlatitude.getText().toString().trim().equals("")) {
+					|| edcategorie.getText().toString().trim().equals("")
+					|| edDegLong.getText().toString().trim().equals("")
+					|| edMinLong.getText().toString().trim().equals("")
+					|| edSecLong.getText().toString().trim().equals("")
+					|| edDegLat.getText().toString().trim().equals("")
+					|| edMinLat.getText().toString().trim().equals("")
+					|| edSecLat.getText().toString().trim().equals("")) {
 				Toast.makeText(CreerActivity.this, champvide, Toast.LENGTH_LONG)
 						.show();
 				return;
@@ -178,6 +375,10 @@ public class CreerActivity extends ActionBarActivity {
 			if (!unique) {
 				Toast.makeText(CreerActivity.this, ptexistedeja,
 						Toast.LENGTH_LONG).show();
+			} else {
+				Toast.makeText(CreerActivity.this, ptcree, Toast.LENGTH_LONG)
+						.show();
+				unique = false;
 			}
 		}
 	}
